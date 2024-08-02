bccd3c84a7ab7d2384da00ce5afad96dc1db211d
Juanito.Lavega <juani.lavega.08@gmail.com>
2024-08-02 09:03:47 -0300

    hola

--- ej22.c
+++ ej22.c
@@ -1,2 +1,13 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include<stdio.h>
+#include<stdlib.h>
+int main(int argc, char *argv[]){
+char *palabra = argv[1];
+int i = 0;
+while(palabra[i] != 0) i++;
+if(i <= 10){
+    printf("%s\n", palabra);
+}else{
+    printf("%c%d%c", palabra[0], i-2, palabra[i-1]);
+}
+return 0;
+}
